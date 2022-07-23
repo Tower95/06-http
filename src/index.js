df75faf63f19a697d3d93c7e201913dd0d2ea65a
Tower95 <torrestmi3@gmail.com>
2022-07-23 00:51:22 -0700

    chain resp Promises

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 
 const joke = 'https://api.chucknorris.io/jokes/random'
 
-fetch(joke).then( (rest) =>
-    rest.json()
-      .then(({id,value})=>{
+// fetch(joke).then( (rest) =>
+//     rest.json()
+//       .then(({id,value})=>{
+//         console.log(value);
+//         console.log(id);
+//       }) 
+// ) 
 
-        console.log(value);
-        console.log(id);
-
-      }) 
-) 
-
-
+fetch(joke)
+  .then(rest => rest.json())
+  .then( ({id,value}) => {
+    console.log(id,value)
+  });
 
