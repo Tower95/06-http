419b66e68d0bd89376dd95278f993fe9c82293c5
Tower95 <torrestmi3@gmail.com>
2022-07-25 22:04:20 -0700

    CRUD

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 'use strict'
-import { init } from './js/usuarios-page'
-import { getUsers } from "./js/http-provider"
+// import { init } from './js/usuarios-page'
+// import { getUsers } from "./js/http-provider"
 
-// import { init } from "./js/joke-page"
+import * as CRUD from './js/crud-provider'
 
-init();
+// CRUD.getUsers(2).then(console.log)
+// CRUD.createUser({
+//   name: 'ismael',
+//   job: 'Programer'
+// }).then(console.log)
+
+// CRUD.updateUser(1, { name: 'Karen', job:'Media Manager'}).then(console.log)
+
+CRUD.deleteUser(2).then(console.log)
--- src/js/crud-provider.js
+++ src/js/crud-provider.js
@@ -3,8 +3,48 @@ const urlCRUD = 'https://reqres.in/api/users';
 
 const getUsers = async (id) => {
 
-  const resp = await fetch(`${ urlCRUD } / ${ id }`);
+  const resp = await fetch(`${urlCRUD}/${id}`);
   const data = await resp.json();
 
-  return data
+  return { data }
+}
+
+const createUser = async (user) => {
+
+  const resp = await fetch(urlCRUD, {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return await resp.json();
+}
+
+const updateUser = async (id, user) => {
+  const resp = await fetch(`${urlCRUD}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'apilcation/json'
+    }
+  });
+
+  return await resp.json();
+}
+
+const deleteUser = async (id) => {
+  const resp = await fetch(`${urlCRUD}/${id}`, {
+    method: 'DELETE'
+  });
+
+  return (resp.ok) ? 'Delete' : 'Not Found';
+}
+
+export {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser
 }
